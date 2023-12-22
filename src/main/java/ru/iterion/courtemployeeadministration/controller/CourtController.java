2cb6199ffea56f4af4f93feaeb7ebeb360cfbfbb
Mikhail Pilugin <tonopod8@yandex.ru>
2023-12-22 21:55:18 +0300

    Поменял эндпоинт в CourtController

--- src/main/java/ru/iterion/courtemployeeadministration/controller/CourtController.java
+++ src/main/java/ru/iterion/courtemployeeadministration/controller/CourtController.java
@@ -3,6 +3,7 @@ package ru.iterion.courtemployeeadministration.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import ru.iterion.courtemployeeadministration.service.CourtServiceImpl;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/court")
 public class CourtController {
     private static final Logger log = LoggerFactory.getLogger(CourtController.class);
 
@@ -24,12 +25,12 @@ public class CourtController {
     }
 
     @GetMapping
-    public List get() {
+    public List get(Model model) {
         log.info("GET request of method get /court");
         return courtService.get();
     }
 
-    @GetMapping("employees/{id}")
+    @GetMapping("/employees/{id}")
     public List getEmployees(@PathVariable String id) {
         log.info("GET request of method getEmployee /employees");
         return courtService.getEmployees(Integer.valueOf(id));
